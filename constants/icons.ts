175402c490e15f97defe445bcc69212401660eab
Aarjav Jain <anshjain638@gmail.com>
2024-08-23 13:48:35 +0530

    Created onBoarding Screen

---
+++ constants/icons.ts
@@ -0,0 +1,8 @@
+import { ImageSourcePropType } from 'react-native';
+
+interface ImageInterface {
+    arrow:ImageSourcePropType;
+}
+export const Icons:ImageInterface = {
+    arrow: require('../assets/right-arrow.png'),
+};
