6798aa30376f8a66b8300722b8861c6f5f64992d
harshit jain <harshit.33569@tmu.ac.in>
2024-08-23 18:40:49 +0530

    Added Settings Screen

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Settings from './screens/Settings';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Settings);
